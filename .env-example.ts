757cf6ccbb44c66a513c086bb106b53c39a648c2
michael spengler <43786652+michael-spengler@users.noreply.github.com>
2022-10-04 05:32:30 +0200

    Update .env-example.ts

--- .env-example.ts
+++ .env-example.ts
@@ -3,5 +3,5 @@ export const pathToCert = '/etc/letsencrypt/live/yourDomain/cert.pem'
 export const pathToCertKey = '/etc/letsencrypt/live/yourDomain/privkey.pem'
 export const telegramBotToken = "yourTelegramBotToken"
 export const apiKey = "defineYourOwnAPIKey"
-export const baseURL = "http://localhost:3080"
+export const baseURL = `http://localhost:${port}`
 export const limitPerHour = 5
